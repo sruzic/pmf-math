198d1782a990b58a1b8b2745d820ce117275c54e
Sanjin <sanjin.ruzic@gmail.com>
2017-01-12 21:29:25 +0100

    Fixed links.

--- aplikacija.php
+++ aplikacija.php
@@ -3,43 +3,43 @@
     <head>
 		<title> Aplikacija za računanje bodova za diplomski studij PMF-MO </title>
         <meta http-equiv="Content-Type" content="text/php; charset=UTF-8" />
-        <link href="stil.css" rel="stylesheet" type="text/css" /> 	
+        <link href="stil.css" rel="stylesheet" type="text/css" />
 		<link rel="shortcut icon" href="./favicon.ico"/>
-    
+
     </head>
     <body>
 		<div class="kontejner">
 			<div class="navigacija">
 				<ul>
-					<li> <a href="index.php"> Zimski Semestar </a></li>
-					<li> <a href="ljetni.html">Ljetni Semestar </a></li>
-					<li> <a href="uvjeti.html">Pravila i Uvjeti </a></li>
+					<li> <a href="index.php"> Zimski </a></li>
+					<li> <a href="ljetni.html">Ljetni </a></li>
+					<li> <a href="uvjeti.html">Uvjeti </a></li>
 					<li> <a class="ovdje" href="aplikacija.php"> Aplikacija </a></li>
 					<li> <a href="kom_teorijska.html"> Diplomski </a></li>
 					<li> <a href="linkovi.html"> Linkovi </a></li>
 					<li> <a href="kontakt.html"> Kontakt </a></li>
 				</ul>
 			</div>
-			<div class="sadrzaj">   
+			<div class="sadrzaj">
 					<form action="./stari.php">
 						<fieldset class="zeleni" id="stari">
 							<legend>Stari program (Mreže Računala obavezan)</legend>
 							<input type="submit" class="button" value="Izračunaj">
 						</fieldset>
-					</form>	
-								
+					</form>
+
 					<form action="./novi.php">
 						<fieldset class="crveni" id="novi">
 							<legend>Novi program (Teorija Brojeva obavezan)</legend>
 							<input type="submit" class="button" value="Izračunaj">
 						</fieldset>
 					</form>
-			</div> 				
-				<?php	
+			</div>
+				<?php
 					 $hit_count = @file_get_contents('count.txt'); // read the hit count from file
 					echo "<div class='vizita'> Izračuna: $hit_count</div>"; //  display the hit count
 				?>
 		</div>
-    
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,133 +2,133 @@
 	<head>
 		<title>Zimski semestar PMF-MO</title>		
 		<meta http-equiv="Content-Type" content="text/php; charset=UTF-8" />
-		<link rel="stylesheet" type="text/css" href="stil.css"/>		
-		<link rel="shortcut icon" href="./favicon.ico"/>		
+		<link rel="stylesheet" type="text/css" href="stil.css"/>
+		<link rel="shortcut icon" href="./favicon.ico"/>
 	</head>
 	<body>
 		<div class="kontejner">
 			<div class="navigacija">
 				<ul>
-					<li> <a class="ovdje" href="index.php"> Zimski Semestar </a></li>
-					<li> <a href="ljetni.html">Ljetni Semestar </a></li>
-					<li> <a href="uvjeti.html">Pravila i Uvjeti </a></li>
+					<li> <a class="ovdje" href="index.php"> Zimski </a></li>
+					<li> <a href="ljetni.html">Ljetni </a></li>
+					<li> <a href="uvjeti.html">Uvjeti </a></li>
 					<li> <a href="aplikacija.php"> Aplikacija </a></li>
 					<li> <a href="kom_teorijska.html"> Diplomski </a></li>
-					<li> <a href="linkovi.html"> Linkovi </a></li> 
-					<li> <a href="kontakt.html"> Kontakt </a></li> 
+					<li> <a href="linkovi.html"> Linkovi </a></li>
+					<li> <a href="kontakt.html"> Kontakt </a></li>
 				</ul>
 			</div>
 			<div class="sadrzaj">
 				<div class="semestar prvi zeleni">
 					<div id="ma1" class="MA2 DISK LOG" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Matematička Analiza 1 </p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/analiza/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/analiza/" > Stranica </a> &nbsp; &nbsp; &nbsp;
 						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=22" > Forum </a> </p>
 					</div>
 					<div id="la1" class="LA2 NUM BIOINF LOG" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Linearna Algebra 1</p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/la/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/la/" > Stranica </a> &nbsp; &nbsp; &nbsp;
 						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=16&amp;sid=2c5a0d3c66d92ce902a418ddc1d808b0" > Forum </a> </p>
 					</div>
 					<div id="em1" class="LA2 EM2 DISK BIOINF" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Elementarna Matematika 1</p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/em/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=10" > Forum </a> </p>		
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/em/" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=10" > Forum </a> </p>
 					</div>
 					<div id="prog1" class="PROG2" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Programiranje 1</p>
 						<p class="info"> <a href="http://degiorgi.math.hr/prog1/" > Stranica </a> &nbsp; &nbsp; &nbsp;
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=32" > Forum </a> </p>		
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=32" > Forum </a> </p>
 					</div>
 				</div>
-				
+
 				<div class="semestar treci crveni">
 					<div id="dfrf" class="ma2 la2 INTRAF ODJ UUDG" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Diferencijalni Račun Funkcija Više Varijabli</p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/difraf/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=18" > Forum </a> </p>		
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/difraf/" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=18" > Forum </a> </p>
 					</div>
 					<div id="disk" class="ma1 em1" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Diskretna Matematika</p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/komb/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=11" > Forum </a> </p>		
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/komb/" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=11" > Forum </a> </p>
 					</div>
 					<div id="vj" class="ma2 STAT MALA OPVIS" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime"> Vjerojatnost</p>
 						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/uuv/" > Stranica </a> &nbsp; &nbsp; &nbsp;
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=20" > Forum </a> </p>		
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=20" > Forum </a> </p>
 					</div>
 					<div id="spa" class="prog2 RP BP MR" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Strukture Podataka i Algoritmi</p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/spa/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=80" > Forum </a> </p>		
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/spa/" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=80" > Forum </a> </p>
 					</div>
 					<div id="eng1" class="ENG2" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Engleski Jezik Struke 1</p>
-						<p class="info"> <a href="http://degiorgi.math.hr/forum/viewforum.php?f=103" > Forum </a> </p>		
+						<p class="info"> <a href="http://degiorgi.math.hr/forum/viewforum.php?f=103" > Forum </a> </p>
 					</div>
 				</div>
-				
+
 				<div class="semestar izborni1 crveni">
 					<div id="fiz1" class="ma2 FIZ2" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Fizika 1</p>
-						<p class="info"> <a href="http://www.phy.pmf.unizg.hr/~zskoko/Fizika_mat_web/Fizika_za_matematicare.html" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=103" > Forum </a> </p>		
+						<p class="info"> <a href="http://www.phy.pmf.unizg.hr/~zskoko/web_sirius/Fizika_za_matematicare.html" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=103" > Forum </a> </p>
 					</div>
 					<div id="mmub" class="ma2" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Matematičko Modeliranje u Biologiji</p>
-						<p class="info"> <a href="http://viveka.math.hr/modeliranje/" > Stranica </a> &nbsp; &nbsp; &nbsp;
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=13" > Forum </a> </p>		
+						<p class="info"> <a href="https://web.math.pmf.unizg.hr/nastava/mmub/index.html" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=13" > Forum </a> </p>
 					</div>
 					<div id="gr" class="prog2" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Građa Računala</p>
-						<p class="info"> <a href="http://brale.math.hr/nastava/gr/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=116" > Forum </a> </p>		
+						<p class="info"> <a href="https://web.math.pmf.unizg.hr/nastava/gr/" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=116" > Forum </a> </p>
 					</div>
 				</div>
-				
+
 				<div class="semestar peti plavi">
 					<div id="as" class="la2" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Algebarske Strukture</p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/alg/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=84" > Forum </a> </p>		
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/alg/" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=84" > Forum </a> </p>
 					</div>
 					<div id="odj" class="dfrf MMF" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Obične Diferencijalne Jednadžbe</p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/odif/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=79" > Forum </a> </p>		
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/odif/" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=79" > Forum </a> </p>
 					</div>
 					<div id="vp" class="la2" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Vektorski Prostori</p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/vekt/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=39" > Forum </a> </p>		
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/vekt/" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=39" > Forum </a> </p>
 					</div>
 					<div id="ts" class="ma2 la2" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Teorija Skupova</p>
-						<p class="info"> <a href="https://sites.google.com/site/mathnastava/home/teorija-skupova" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=46" > Forum </a> </p>		
+						<p class="info"> <a href="https://web.math.pmf.unizg.hr/nastava/ts/index.html" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=46" > Forum </a> </p>
 					</div>
 				</div>
-				
+
 				<div class="semestar izborni3 plavi">
 					<div id="log" class="ma1 la1" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Matematička Logika</p>
 						<p class="info"> <a href="https://sites.google.com/site/mathnastava/home/matematicka-logika" > Stranica </a> &nbsp; &nbsp; &nbsp;
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=100" > Forum </a> </p>		
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=100" > Forum </a> </p>
 					</div>
 					<div id="iter" class="num" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Iterativne Metode</p>
 						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/iter/" > Stranica </a> &nbsp; &nbsp; &nbsp;
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=100	" > Forum </a> </p>		
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=100	" > Forum </a> </p>
 					</div>
 					<div id="mala" class="intraf vj" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Markovljevi Lanci</p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/mala/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=100" > Forum </a> </p>		
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/mala/" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=100" > Forum </a> </p>
 					</div>
 					<div id="ep" class="la2" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Euklidski Prostori</p>
 						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/eukl/" > Stranica </a> &nbsp; &nbsp; &nbsp;
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=40" > Forum </a> </p>		
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=40" > Forum </a> </p>
 					</div>
 					<div id="mr" class="spa" onmouseover="oboji(this)" onmouseout="izbrisi(this)"><p class="ime">Mreže Računala</p>
-						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/mreze/" > Stranica </a> &nbsp; &nbsp; &nbsp; 
-						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=101" > Forum </a> </p>		
+						<p class="info"> <a href="http://web.math.pmf.unizg.hr/nastava/mreze/" > Stranica </a> &nbsp; &nbsp; &nbsp;
+						<a href="http://degiorgi.math.hr/forum/viewforum.php?f=101" > Forum </a> </p>
 					</div>
 				</div>
-			</div>	
+			</div>
 
 			<div id="posjeta"><!-- hitwebcounter Code START -->
 				<a href="http://www.hitwebcounter.com/" target="_blank">
 				<img src="http://hitwebcounter.com/counter/counter.php?page=4713226&style=0008&nbdigits=4&type=ip&initCount=0" title="" Alt=""   border="0" >
 				</a>
 			</div>
-			<?php	
+			<?php
 				$posjet = @file_get_contents('posjeta.txt'); // read the hit count from file
-				$posjet = $posjet + 1; // increment the hit count by 1	   
+				$posjet = $posjet + 1; // increment the hit count by 1
 				@file_put_contents('posjeta.txt', $posjet); // store
 			?>
-			
-		</div>	
+
+		</div>
 		</body>
 </html>
